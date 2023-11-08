e927339cd7b8a320dd713bc731e5b9b86d2ee3f2
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-11-08 12:00:27 -0600

    Delete ShellSync directory

--- ShellSync/Pages/Tools.ps1
+++
@@ -1,9 +0,0 @@
-	Add-PodeWebPage -Name 'Tools' -Icon 'Settings' -ScriptBlock {
-	New-PodeWebContainer -NoBackground -Content @(
-		New-PodeWebTextbox -Name 'Content'
-
-		New-PodeWebButton -Name 'Update Textbox' -ScriptBlock {
-			Update-PodeWebTextbox -Name 'Content' -Value ([datetime]::Now.ToString())
-		}
-	)
-	}
